add8e896a122f58606ec10e6d3615e853ac6178f
Adam <atmostadam@gmail.com>
2022-11-30 20:17:44 -0500

    Add request logging

---
+++ src/main/java/com/atmostadam/cats/api/configuration/CatLoggingConfiguration.java
@@ -0,0 +1,19 @@
+package com.atmostadam.cats.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class CatLoggingConfiguration {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(false);
+        loggingFilter.setIncludeHeaders(false);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(10000);
+        return loggingFilter;
+    }
+}
