5864547a1d590001d8e37fedc95089ee9e73fa37
Adam <atmostadam@gmail.com>
2022-12-22 10:58:05 -0500

    More github configurations.

---
+++ src/main/java/com/atmostadam/cats/api/CatApiGithub.java
@@ -0,0 +1,11 @@
+package com.atmostadam.cats.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatApiGithub {
+    public static void main(String[] args) {
+        SpringApplication.run(CatApiGithub.class, args);
+    }
+}
